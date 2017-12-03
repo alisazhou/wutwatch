cb7e6d3239c2a947abc571ef62457aeceb49e463
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-03 13:35:01 -0800

    add browsable endpoint for Movie

--- wutwatch/wutwatch/urls.py
+++ wutwatch/wutwatch/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from movies.views import MovieViewSet
 from profiles.views import ProfileViewSet
 
 
 router = DefaultRouter()
+router.register(r'movies', MovieViewSet, 'movie')
 router.register(r'profiles', ProfileViewSet, 'profile')
 
 
