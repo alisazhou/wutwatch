67461cecb49d0cb8fc3e835ec348bd4775a0580f
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-03 14:16:21 -0800

    create User when creating Profile

--- wutwatch/profiles/serializers.py
+++ wutwatch/profiles/serializers.py
@@ -6,4 +6,4 @@ from .models import Profile
 class ProfileSerializers(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id', 'email', 'full_name')
+        fields = ('id', 'email', 'full_name', 'user')
--- wutwatch/profiles/views.py
+++ wutwatch/profiles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 
 from .models import Profile
@@ -7,3 +8,12 @@ from .serializers import ProfileSerializers
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializers
+
+    def create(self, request, *args, **kwargs):
+        # create a user using the email as username
+        user = User.objects.create(username=request.data.get('email'), **request.data)
+        # create a Profile instance with the newly created user
+        request.data['user'] = user.id
+        response = super().create(request, *args, **kwargs)
+
+        return response
