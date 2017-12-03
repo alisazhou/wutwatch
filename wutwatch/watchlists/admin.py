3803011ca1122d2a31cd4ef8eb87f849baaed500
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-03 13:06:38 -0800

    add WatchList

---
+++ wutwatch/watchlists/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import WatchList
+
+
+@admin.register(WatchList)
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'id')
---
+++ wutwatch/watchlists/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from movies.models import Movie
+from profiles.models import Profile
+
+
+class WatchList(models.Model):
+    name = models.CharField(max_length=100)
+    watchers = models.ManyToManyField(Profile)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return name
