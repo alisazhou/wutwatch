5af717599d7305826fabeeef35c0d56595255e93
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-03 13:29:11 -0800

    add browsable endpoint for Profile

--- wutwatch/profiles/views.py
+++ wutwatch/profiles/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Profile
+from .serializers import ProfileSerializers
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializers
