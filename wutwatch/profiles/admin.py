3098076a3216f7390aa4e91a2451bca39402a20b
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-03 13:06:25 -0800

    add properties to Profile

--- wutwatch/profiles/admin.py
+++ wutwatch/profiles/admin.py
@@ -5,4 +5,4 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'id')
+    list_display = ('__str__', 'id', 'email')
--- wutwatch/profiles/models.py
+++ wutwatch/profiles/models.py
@@ -6,5 +6,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # TODO: add photo field
 
-    def __str__(self):
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def full_name(self):
         return '{} {}'.format(self.user.first_name, self.user.last_name)
+
+    def __str__(self):
+        return self.full_name
