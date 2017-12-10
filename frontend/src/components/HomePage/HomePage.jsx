1591e1617a8df1b2aa3139f106a00f6010335747
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-10 13:54:26 -0800

    combine Login and Create user forms

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
-import CreateUserForm from './CreateUserForm';
-import LoginUserForm from './LoginUserForm';
+import LoginPage from './LoginPage';
 import Dashboard from '../Dashboard';
 
 
@@ -12,11 +11,9 @@ class HomePage extends React.Component {
             <div>
                 <Link to="/">Dashboard</Link>
                 <Link to="/login">Login</Link>
-                <Link to="/signup">Sign Up</Link>
                 <Switch>
                     <Route exact path="/" component={Dashboard} />
-                    <Route exact path="/login" component={LoginUserForm} />
-                    <Route exact path="/signup" component={CreateUserForm} />
+                    <Route exact path="/login" component={LoginPage} />
                 </Switch>
             </div>
         );
---
+++ frontend/src/components/HomePage/LoginPage.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+
+import { createUserAction, loginUserAction } from '../../state/actions/authActions';
+
+
+class LoginPage extends React.Component {
+    state = {
+        showLoginTab: true,
+    };
+
+    handleLoginTabClick = showLoginTab => {
+        this.setState({ showLoginTab });
+    }
+
+    get tabs() {
+        return (
+            <div>
+                <span onClick={() => this.handleLoginTabClick(true)}>I have an account</span>
+                <span onClick={() => this.handleLoginTabClick(false)}>Sign Up</span>
+            </div>
+        );
+    };
+
+    render () {
+        if (this.props.isAuthenticated || localStorage.hasOwnProperty('token')) {
+            return <Redirect to="/" />
+        } else if (this.state.showLoginTab) {
+            return (
+                <div>
+                    {this.tabs}
+                    <form onSubmit={this.props.handleSubmit(this.props.loginUser)}>
+                        email: <Field component='input' type='text' name='username' />
+                        password: <Field component='input' type='password' name='password' />
+                        <button type='submit'>submit</button>
+                    </form>
+                </div>
+            );
+        }
+        return (
+            <div>
+                {this.tabs}
+                <form onSubmit={this.props.handleSubmit(this.props.createUser)}>
+                    first name: <Field component='input' type='text' name='first_name' />
+                    last name: <Field component='input' type='text' name='last_name' />
+                    email: <Field component='input' type='text' name='email' />
+                    password: <Field component='input' type='password' name='password' />
+                    <button type='submit'>submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth && state.auth.isAuthenticated,
+});
+
+const mapDispatchToProps = dispatch => ({
+    createUser: creds => {
+        dispatch(createUserAction(creds));
+    },
+    loginUser: creds => {
+        dispatch(loginUserAction(creds));
+    },
+});
+
+const ConnectedLoginPage = connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+
+const WrappedLoginPage = reduxForm({
+    form: 'createNewUser'
+})(ConnectedLoginPage);
+
+export default WrappedLoginPage;
