733eeb510126febf5f051866e4d9a69ca2416f9f
alisa.zhou <alisa.zhou@zanbato.com>
2017-12-10 20:21:35 -0800

    fix watchlist backend views

--- wutwatch/watchlists/models.py
+++ wutwatch/watchlists/models.py
@@ -6,8 +6,8 @@ from profiles.models import Profile
 
 class WatchList(models.Model):
     name = models.CharField(max_length=100)
-    watchers = models.ManyToManyField(Profile)
-    movies = models.ManyToManyField(Movie)
+    watchers = models.ManyToManyField(Profile, blank=True)
+    movies = models.ManyToManyField(Movie, blank=True)
 
     def __str__(self):
         return self.name
--- wutwatch/watchlists/views.py
+++ wutwatch/watchlists/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
+from profiles.models import Profile
 from .models import WatchList
 from .serializers import WatchListSerializer
 
@@ -7,3 +9,33 @@ from .serializers import WatchListSerializer
 class WatchListViewSet(viewsets.ModelViewSet):
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        # get the newly-created WatchList instance
+        new_watchlist = WatchList.objects.get(id=response.data['id'])
+        # associate the new watchlist with the current user
+        user_profile = Profile.objects.get(user=request.user)
+        new_watchlist.watchers.add(user_profile)
+        new_watchlist.save()
+
+        response = Response(self.get_serializer(new_watchlist).data, status=status.HTTP_201_CREATED)
+
+        return response
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        watcher_email = request.data.get('watcher')
+        if watcher_email:
+            try:
+                watcher = Profile.objects.get(user__username=watcher_email)
+            except Profile.DoesNotExcept:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            instance.watchers.add(watcher)
+
+        instance.save()
+
+        return Response(self.get_serializer(instance).data)
