4f8378a84714a46a1f34ae298ec4e8042f19ae39
alisa.zhou <alisa.zhou@zanbato.com>
2018-01-05 13:32:10 -0700

    refactor PickMovieButton

--- frontend/src/components/Movies/PickMovieButton.jsx
+++ frontend/src/components/Movies/PickMovieButton.jsx
@@ -3,7 +3,10 @@ import _ from 'lodash';
 import moment from 'moment';
 import { connect } from 'react-redux';
 
-import { buttonMedium, typographyTitleOnLight } from '../cssConstants';
+import {
+    backgroundTitle, buttonMedium,
+    typographyTitle, typographyTitleOnLight,
+} from '../cssConstants';
 import {
     updateSelectedMovieActionCreator,
     updateJustPickedActionCreator,
@@ -25,26 +28,29 @@ const style = {
 class PickMovieButton extends React.Component {
     handlePickMovie = () => {
         const selectedMovie = _.sample(this.props.movies);
-        // TODO: show error if no movies to choose from
-        if (selectedMovie) {
-            localStorage.setItem('lastPickedTime', moment().format());
-            localStorage.setItem('selectedMovieName', selectedMovie.name);
-            localStorage.setItem('selectedMoviePosterUrl', selectedMovie.poster_url);
-            localStorage.setItem('selectedMovieReleaseDate', selectedMovie.release_date);
 
-            this.props.updateJustPicked();
-            this.props.updateSelectedMovie();
-        }
+        localStorage.setItem('lastPickedTime', moment().format());
+        localStorage.setItem('selectedMovie', JSON.stringify(selectedMovie));
+
+        this.props.updateJustPicked();
+        this.props.updateSelectedMovie();
     }
 
     render() {
-        if (this.props.justPicked) {
-            return null;
+        let onClick = this.handlePickMovie;
+        let finalStyle = _.clone(style);
+        let divText = 'wut to watch now?';
+
+        if (_.isEmpty(this.props.movies)) {
+            // if no movies to pick from
+            onClick = _.noop;
+            divText = 'no movies to pick from';
+            _.assign(finalStyle, typographyTitle, { background: backgroundTitle, cursor: 'default' });
         }
 
         return (
-            <div onClick={this.handlePickMovie} style={style}>
-                wut to watch now?
+            <div onClick={onClick} style={finalStyle}>
+                {divText}
             </div>
         );
     }
