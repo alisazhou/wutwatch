ec49ff3062e5fc98f6a1131b085f82503f89c306
alisa.zhou <alisa.zhou@zanbato.com>
2018-01-04 18:36:06 -0700

    move pre-loading to Dashboard from HomePage

--- frontend/src/components/HomePage/Dashboard.jsx
+++ frontend/src/components/HomePage/Dashboard.jsx
@@ -1,15 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import requireAuth from '../requireAuth';
 import Movies from '../Movies/Movies';
+import Navbar from '../Navbar/Navbar';
 import Watchlists from '../Watchlists/Watchlists';
+import { loadMoviesActionCreator } from '../../state/actions/movieActions';
+import { loadWatchHistoriesActionCreator } from '../../state/actions/watchHistoryActions';
+import { loadWatchlistsActionCreator } from '../../state/actions/watchlistActions';
 
 
-const Dashboard = props => (
-    <div>
-        <Watchlists />
-        <Movies />
-    </div>
-);
+class Dashboard extends React.Component {
+    componentWillMount() {
+        if (_.isEmpty(this.props.movies)) {
+            this.props.loadMovies();
+        }
 
-export default requireAuth(Dashboard);
+        if (_.isEmpty(this.props.watchHistories)) {
+            this.props.loadWatchHistories();
+        }
+
+        if (_.isEmpty(this.props.watchlists)) {
+            this.props.loadWatchlists();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Watchlists />
+                <Movies />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    movies: state.movies.movies,
+    watchHistories: state.watchHistories.watchHistories,
+    watchlists: state.watchlists.watchlists,
+});
+
+const mapDispatchToProps = dispatch => ({
+    loadMovies: () => {
+        dispatch(loadMoviesActionCreator());
+    },
+    loadWatchHistories: () => {
+        dispatch(loadWatchHistoriesActionCreator());
+    },
+    loadWatchlists: () => {
+        dispatch(loadWatchlistsActionCreator());
+    },
+});
+
+const ConnectedDashboard = connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+
+export default requireAuth(ConnectedDashboard);
--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import Dashboard from './Dashboard';
-import Navbar from '../Navbar/Navbar';
-import { loadMoviesActionCreator } from '../../state/actions/movieActions';
-import { loadWatchHistoriesActionCreator } from '../../state/actions/watchHistoryActions';
-import { loadWatchlistsActionCreator } from '../../state/actions/watchlistActions';
 
 
 const style = {
@@ -13,48 +8,14 @@ const style = {
 };
 
 class HomePage extends React.Component {
-    componentWillMount() {
-        if (_.isEmpty(this.props.movies)) {
-            this.props.loadMovies();
-        }
-
-        if (_.isEmpty(this.props.watchHistories)) {
-            this.props.loadWatchHistories();
-        }
-
-        if (_.isEmpty(this.props.watchlists)) {
-            this.props.loadWatchlists();
-        }
-    }
 
     render() {
         return (
             <div style={style}>
-                <Navbar />
                 <Dashboard />
             </div>
         );
     }
 };
 
-const mapStateToProps = state => ({
-    movies: state.movies.movies,
-    watchHistories: state.watchHistories.watchHistories,
-    watchlists: state.watchlists.watchlists,
-});
-
-const mapDispatchToProps = dispatch => ({
-    loadMovies: () => {
-        dispatch(loadMoviesActionCreator());
-    },
-    loadWatchHistories: () => {
-        dispatch(loadWatchHistoriesActionCreator());
-    },
-    loadWatchlists: () => {
-        dispatch(loadWatchlistsActionCreator());
-    },
-});
-
-const ConnectedPage = connect(mapStateToProps, mapDispatchToProps)(HomePage);
-
-export default ConnectedPage;
+export default HomePage;
