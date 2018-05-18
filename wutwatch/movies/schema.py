6abb899df15e7b8d1feae8a957c14711fc0afad5
alisazhou <alisa.zwy@gmail.com>
2018-05-18 15:39:59 -0700

    use django filters in movies and profiles schemas

--- wutwatch/movies/schema.py
+++ wutwatch/movies/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from .models import Movie
@@ -7,20 +8,13 @@ from .models import Movie
 class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
+        filter_fields = {
+            'name': ['icontains'],  # name_Icontains
+            'release_date': ['year', 'year__gt'],  # releaseDate_Year_Gt
+        }
+        interfaces = (graphene.Node,)
 
 
 class Query(object):
-    movie = graphene.Field(MovieType, id=graphene.Int(), name=graphene.String())
-    all_movies = graphene.List(MovieType)
-
-    def resolve_movie(self, info, **kwargs):
-        id = kwargs.get('id')
-        if id is not None:
-            return Movie.objects.get(id=id)
-
-        name = kwargs.get('name')
-        if name is not None:
-            return Movie.objects.get(name__iexact=name)
-
-    def resolve_all_movies(self, info, **kwargs):
-        return Movie.objects.all()
+    movie = graphene.Node.Field(MovieType)
+    all_movies = DjangoFilterConnectionField(MovieType)
--- wutwatch/profiles/schema.py
+++ wutwatch/profiles/schema.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from .models import Profile
@@ -13,20 +14,12 @@ class UserType(DjangoObjectType):
 class ProfileType(DjangoObjectType):
     class Meta:
         model = Profile
+        filter_fields = {
+            'user__first_name': ['iexact'],
+        }
+        interfaces = (graphene.Node, )
 
 
 class Query(object):
-    profile = graphene.Field(ProfileType, id=graphene.Int(), email=graphene.String())
-    all_profiles = graphene.List(ProfileType)
-
-    def resolve_profile(self, info, **kwargs):
-        id = kwargs.get('id')
-        if id is not None:
-            return Profile.objects.get(id=id)
-
-        email = kwargs.get('email')
-        if email is not None:
-            return Profile.objects.get(user__email=email)
-
-    def resolve_all_profiles(self, info, **kwargs):
-        return Profile.objects.select_related('user').all()
+    profile = graphene.Node.Field(ProfileType)
+    all_profiles = DjangoFilterConnectionField(ProfileType)
