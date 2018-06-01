06679806b93d57dfbe986f418e3ba9b8a6216dca
alisazhou <alisa.zwy@gmail.com>
2018-06-01 11:16:08 -0700

    require auth in graphql endpoint

--- wutwatch/wutwatch/urls.py
+++ wutwatch/wutwatch/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
@@ -17,12 +18,17 @@ router.register(r'watchhistories', WatchHistoryViewSet, base_name='watchhistory'
 router.register(r'watchlists', WatchListViewSet, 'watchlist')
 
 
+class LoginRequiredGraphQLView(LoginRequiredMixin, GraphQLView):
+    # NOTE: currently still using Django's session auth, so log in via admin page first
+    pass
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/search-movie/', search_movie),
     path('api/authtoken/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  # for logging in on browsable endpoints
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', LoginRequiredGraphQLView.as_view(graphiql=True)),
     re_path('^.*', TemplateView.as_view(template_name='index.html')),
 ]
