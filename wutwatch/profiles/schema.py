872692a941ac320a509d964ee621f92cbe65f310
alisazhou <alisa.zwy@gmail.com>
2018-06-01 11:15:42 -0700

    use custom filter class in profiles

--- wutwatch/profiles/schema.py
+++ wutwatch/profiles/schema.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+import django_filters
 import graphene
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
@@ -14,12 +15,25 @@ class UserType(DjangoObjectType):
 class ProfileType(DjangoObjectType):
     class Meta:
         model = Profile
-        filter_fields = {
-            'user__first_name': ['iexact'],
-        }
         interfaces = (graphene.Node, )
+
+
+class ProfileFilter(django_filters.FilterSet):
+    email = django_filters.CharFilter(field_name='user__email', lookup_expr='iexact')
+    first_name = django_filters.CharFilter(field_name='user__first_name', lookup_expr='iexact')
+    # TODO: how do i use django filter for 'first_name + last_name' icontains query?
+
+    class Meta:
+        model = Profile
+        fields = ('email', 'first_name', )
+
+    @property
+    def qs(self):
+        return super(ProfileFilter, self).qs.\
+            prefetch_related('watchlists__watchers__user')\
+            .filter(watchlists__watchers__user=self.request.user).distinct()
 
 
 class Query(object):
     profile = graphene.Node.Field(ProfileType)
-    all_profiles = DjangoFilterConnectionField(ProfileType)
+    all_profiles = DjangoFilterConnectionField(ProfileType, filterset_class=ProfileFilter)
