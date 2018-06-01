c1c106270a8993dad234bf47c4f3b506c0684129
alisazhou <alisa.zwy@gmail.com>
2018-06-01 14:09:18 -0700

    use custom filters in watchlists

--- wutwatch/watchlists/schema.py
+++ wutwatch/watchlists/schema.py
@@ -1,3 +1,4 @@
+import django_filters
 import graphene
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
@@ -9,25 +10,37 @@ from .models import WatchHistory, WatchList
 class WatchHistoryType(DjangoObjectType):
     class Meta:
         model = WatchHistory
-        filter_fields = {
-            'movie__name': ['icontains'],
-            'watchlist__name': ['iexact'],
-        }
-        interfaces = (graphene.Node,)
+        interfaces = (graphene.Node, )
+
+
+class WatchHistoryFilter(django_filters.FilterSet):
+    movie = django_filters.CharFilter(field_name='movie__name', lookup_expr='icontains')
+    watchlist = django_filters.CharFilter(field_name='watchlist__name', lookup_expr='iexact')
+
+    class Meta:
+        model = WatchHistory
+        fields = ('movie', 'watchlist',)
 
 
 class WatchListType(DjangoObjectType):
     class Meta:
         model = WatchList
-        filter_fields = {
-            'name': ['iexact'],
-        }
         interfaces = (graphene.Node,)
+
+
+class WatchListFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = WatchHistory
+        fields = ('name',)
 
 
 class Query(object):
     watch_history = graphene.Node.Field(WatchHistoryType)
-    all_watch_histories = DjangoFilterConnectionField(WatchHistoryType)
+    all_watch_histories = DjangoFilterConnectionField(
+        WatchHistoryType,
+        filterset_class=WatchHistoryFilter)
 
     watchlist = graphene.Node.Field(WatchListType)
-    all_watchlists = DjangoFilterConnectionField(WatchListType)
+    all_watchlists = DjangoFilterConnectionField(WatchListType, filterset_class=WatchListFilter)
