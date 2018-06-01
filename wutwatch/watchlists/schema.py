60ffc9418ee0bee652eab9c2435b828f3e0412b0
alisazhou <alisa.zwy@gmail.com>
2018-06-01 11:43:46 -0700

    use simple filters in watchlists

--- wutwatch/watchlists/schema.py
+++ wutwatch/watchlists/schema.py
@@ -1,5 +1,7 @@
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
+
 
 from .models import WatchHistory, WatchList
 
@@ -7,44 +9,25 @@ from .models import WatchHistory, WatchList
 class WatchHistoryType(DjangoObjectType):
     class Meta:
         model = WatchHistory
+        filter_fields = {
+            'movie__name': ['icontains'],
+            'watchlist__name': ['iexact'],
+        }
+        interfaces = (graphene.Node,)
 
 
 class WatchListType(DjangoObjectType):
     class Meta:
         model = WatchList
+        filter_fields = {
+            'name': ['iexact'],
+        }
+        interfaces = (graphene.Node,)
 
 
 class Query(object):
-    watch_history = graphene.Field(WatchHistoryType,
-                                   id=graphene.Int(),
-                                   movie_id=graphene.Int(),
-                                   watchlist_id=graphene.Int())
-    all_watch_histories = graphene.List(WatchHistoryType)
+    watch_history = graphene.Node.Field(WatchHistoryType)
+    all_watch_histories = DjangoFilterConnectionField(WatchHistoryType)
 
-    watchlist = graphene.Field(WatchListType, id=graphene.Int(), name=graphene.String())
-    all_watchlists = graphene.List(WatchListType)
-
-    def resolve_watch_history(self, info, **kwargs):
-        id = kwargs.get('id')
-        if id is not None:
-            return WatchHistory.objects.get(id=id)
-
-        movie_id = kwargs.get('movie_id')
-        watchlist_id = kwargs.get('watchlist_id')
-        if movie_id is not None and watchlist_id is not None:
-            return WatchHistory.objects.get(movie=movie_id, watchlist=watchlist_id)
-
-    def resolve_all_watch_histories(self, info, **kwargs):
-        return WatchHistory.objects.all()
-
-    def resolve_watchlist(self, info, **kwargs):
-        id = kwargs.get('id')
-        if id is not None:
-            return WatchList.objects.get(id=id)
-
-        name = kwargs.get('name')
-        if name is not None:
-            return WatchList.objects.get(name__iexact=name)
-
-    def resolve_all_watchlists(self, info, **kwargs):
-        return WatchList.objects.all()
+    watchlist = graphene.Node.Field(WatchListType)
+    all_watchlists = DjangoFilterConnectionField(WatchListType)
