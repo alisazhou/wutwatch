b8261bdd860e31a2e7c4a998b494da925d0ca4aa
alisazhou <alisa.zwy@gmail.com>
2018-06-15 13:14:52 -0700

    add (very) basic apollo client

--- wutwatch/wutwatch/urls.py
+++ wutwatch/wutwatch/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import include, path, re_path
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 from rest_framework.authtoken import views
@@ -20,6 +21,7 @@ router.register(r'watchlists', WatchListViewSet, 'watchlist')
 
 class LoginRequiredGraphQLView(LoginRequiredMixin, GraphQLView):
     # NOTE: currently still using Django's session auth, so log in via admin page first
+    # TODO: use this instead of GraphQLView in urlpatterns to implement
     pass
 
 
@@ -29,6 +31,7 @@ urlpatterns = [
     path('api/search-movie/', search_movie),
     path('api/authtoken/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  # for logging in on browsable endpoints
-    path('graphql/', LoginRequiredGraphQLView.as_view(graphiql=True)),
+    # TODO: look in adding csrf in the request header for production
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     re_path('^.*', TemplateView.as_view(template_name='index.html')),
 ]
